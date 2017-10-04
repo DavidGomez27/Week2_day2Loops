bdad581caa366e6e502da95d851b09c0fe7dd59b
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-04 10:52:12 -0400

    while loops

--- Week2_day2Loops/Program.cs
+++ Week2_day2Loops/Program.cs
@@ -74,11 +74,74 @@ namespace Week2_day2Loops
 
 
 
-            int[] numbers = { 3, 5, 7, 11, 15 };
-            foreach (int luckyNumbers in numbers)
+            //int[] numbers = { 3, 5, 7, 11, 15 };
+            //foreach (int luckyNumbers in numbers)
+            //{
+            //    Console.WriteLine("Your Lucky Number is: " + (luckyNumbers));
+            //}
+
+
+
+            //for (int i = 10; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine("do you want to play again? (yes/no)");
+            //string playAgain = Console.ReadLine();
+            //while(playAgain == "yes")
+            //{
+            //    Console.WriteLine("its a rematch!");
+            //    Console.WriteLine("do you want to play again? (yes/no)");
+            //    playAgain = Console.ReadLine();
+            //}
+
+
+
+            //Console.WriteLine("Would you like to check in patient for appoinment? (yes/no)");
+            //string checkIn = Console.ReadLine();
+            //while(checkIn == "yes")
+            //{
+            //    Console.WriteLine("PATIENT CHECK IN SYSTEM");
+
+            //    Console.WriteLine("what is your full name?");
+            //    Console.ReadLine().ToLower();
+            //    Console.WriteLine("what is your social?");
+            //    int userId = int.Parse(Console.ReadLine().ToLower());
+            //    Console.WriteLine("what time is your appointment?");
+            //    Console.ReadLine().ToLower();
+
+            //    Console.WriteLine("would you like to check in for appointment?");
+            //    checkIn = Console.ReadLine().ToLower();
+
+            //}
+
+
+
+
+            string playAgain;
+            do
             {
-                Console.WriteLine("Your Lucky Number is: " + (luckyNumbers));
+                
+                Console.WriteLine("Great game!");
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                playAgain = Console.ReadLine();
             }
+            while (playAgain == "yes");
+
+            Console.WriteLine("Thanks for playing the game");
+
+
+
+
+
+
+
+
+
+
+
 
 
 
